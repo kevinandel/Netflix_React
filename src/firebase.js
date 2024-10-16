6314926273c1d0b7aa120b1942820beed33ceeda
Kevin Andel Josy <kevinandeljosy11@gmail.com>
2024-10-16 20:40:07 +0530

    small formatting

--- src/firebase.js
+++ src/firebase.js
@@ -39,7 +39,7 @@ const signup = async (name, email, password) => {
     });
   } catch (error) {
     console.log(error);
-    toast.error(error.code.split('/')[1].split('-').join(" "));
+    toast.error(error.code.split("/")[1].split("-").join(" "));
   }
 };
 
@@ -48,7 +48,7 @@ const login = async (email, password) => {
     await signInWithEmailAndPassword(auth, email, password);
   } catch (error) {
     console.log(error);
-    toast.error(error.code.split('/')[1].split('-').join(" "));
+    toast.error(error.code.split("/")[1].split("-").join(" "));
   }
 };
 
